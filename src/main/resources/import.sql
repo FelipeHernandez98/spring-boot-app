7cef7c9c21675758a89c75a682c6a397b1e329d1
unknown <andresfelipehc@ufps.edu.co>
2022-07-29 09:31:09 -0500

    implementando la vista de listar con thymeleaf y bootstrap

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/*Populate tables*/
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Hernandez', 'andres@correo.com', '2022-08-29');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Pepito', 'Perez', 'pepo@correo.com', '2022-08-29');
