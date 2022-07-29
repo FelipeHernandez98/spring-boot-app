68d875bbe8d36956e9734884119a9e0af766e36a
unknown <andresfelipehc@ufps.edu.co>
2022-07-29 16:03:05 -0500

    modificanco la conexion a mySql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 /*Populate tables*/
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES( 'Andres', 'Hernandez', 'andres@correo.com', '2022-08-29');
-INSERT INTO clientes ( nombre, apellido, email, create_at) VALUES( 'Pepito', 'Perez', 'pepo@correo.com', '2022-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Hernandez', 'andres@correo.com', '2022-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Angie', 'Rodriguez', 'angie@correo.com', '2022-08-29');
