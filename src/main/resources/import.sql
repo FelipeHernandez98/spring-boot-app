bba5970611d527bb881b47b6324673c45c53bc50
unknown <andresfelipehc@ufps.edu.co>
2022-07-30 15:59:15 -0500

    creando datos para la tabla productos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,11 @@
 /*Populate tables*/
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Andres', 'Hernandez', 'andres@correo.com', '2022-08-29', '');
 INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Angie', 'Rodriguez', 'angie@correo.com', '2022-08-29', '');
+
+
+/* Populate tabla productos */
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Panasonic Pantalla LCD', 1548923 , NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara Digital', 358999 , NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple iPod', 854998, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook', 689998 , NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES ('HP portatil', 978998 , NOW());
