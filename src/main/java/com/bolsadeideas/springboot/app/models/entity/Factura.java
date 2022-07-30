3c2fbc711059612f7ac48bd0c3eba47137cf8975
unknown <andresfelipehc@ufps.edu.co>
2022-07-30 15:01:28 -0500

    creando la clase ItemFactura y su relacion unidireccional con Factura

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -2,14 +2,18 @@ package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -41,6 +45,11 @@ public class Factura implements Serializable {
 	//Many es la primera que hace referencia a la clase donde estamos, siempre va de primero la clase en la que estamos
 	@ManyToOne(fetch=FetchType.LAZY) 
 	private Cliente cliente;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="factura_id")//Se coloca para generar el factura_id en facturas_item
+	private List<ItemFactura> items;
+	
 
 	public Long getId() {
 		return id;
@@ -82,4 +91,17 @@ public class Factura implements Serializable {
 		this.cliente = cliente;
 	}
 
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+	
+	public void addItemFactura(ItemFactura item) {
+		this.items.add(item);
+	}
+
+	
 }
