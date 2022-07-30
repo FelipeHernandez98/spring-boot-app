7619a58652b0c7832b57dafc5f0d08deb97a2b0c
unknown <andresfelipehc@ufps.edu.co>
2022-07-30 11:37:40 -0500

    agregando funcionalidad de subir imagenes y creando su vista

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,18 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping(value="/ver/{id}")
+	public String ver(@PathVariable(value = "id")Long id, Map<String, Object> model, RedirectAttributes flash) {
+		 Cliente cliente = clienteService.findOne(id);
+		 if(cliente == null) {
+			 flash.addFlashAttribute("error", "El cliente no existe");
+			 return "redirect:/listar";
+		 }
+		 model.put("cliente", cliente);
+		 model.put("titulo", "Detalle cliente: " +cliente.getNombre() + cliente.getApellido());
+		return "ver";
+	}
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
 	public String Listar(Model model) {
