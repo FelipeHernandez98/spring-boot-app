7b75c92beff22c0b6cfc664939f50bbe5eb33784
unknown <andresfelipehc@ufps.edu.co>
2022-08-01 11:33:11 -0500

    creando la vista de ver facturas y la de crar facturas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,3 +9,10 @@ INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Camara Digital',
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Apple iPod', 854998, NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('Sony Notebook', 689998 , NOW());
 INSERT INTO productos (nombre, precio, create_at) VALUES ('HP portatil', 978998 , NOW());
+
+/* Populate tabla facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES ('Fact equipos', null, 1, NOW())
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2, 1, 2);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 5);
