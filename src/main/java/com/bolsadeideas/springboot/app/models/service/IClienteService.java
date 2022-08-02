e2fac4bdfad2789a335a5966a5cbaaa297c6d886
unknown <andresfelipehc@ufps.edu.co>
2022-08-02 13:16:31 -0500

    creando interface para Factura, e implmentandola con crudRepository

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Factura;
 import com.bolsadeideas.springboot.app.models.entity.Producto;
 
 public interface IClienteService {
@@ -19,5 +20,7 @@ public interface IClienteService {
 	public void delete(Long id);
 	
 	public List<Producto> findByName( String term);
+	
+	public void saveFactura(Factura factura);
 
 }
