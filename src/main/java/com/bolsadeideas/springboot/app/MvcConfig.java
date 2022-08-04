055f7f1c09faeada16f42b41571d9a8a7f6799ac
unknown <andresfelipehc@ufps.edu.co>
2022-08-04 11:57:04 -0500

    dejando plasmados los metodos(cambio de locale e interceptor) para cambio de idioma

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,9 +1,17 @@
 package com.bolsadeideas.springboot.app;
 
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
@@ -19,5 +27,31 @@ public class MvcConfig implements WebMvcConfigurer{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
 	}
+	
+	@Bean
+	public LocaleResolver localResolver() {
+		
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		return localeResolver;
+		
+	}
+	
+	//----------------- metodos para cambiar de idioma que no se implementara solo se deja plasmado --------//
+	@Bean
+	public LocaleChangeInterceptor localeChangeIterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		
+		return localeChangeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		WebMvcConfigurer.super.addInterceptors(registry);
+		registry.addInterceptor(localeChangeIterceptor());
+	}
+	
+	
 	
 }
