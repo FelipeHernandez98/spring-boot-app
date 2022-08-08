5fbc1075777aecc5af65f5933274d33459d80daf
unknown <andresfelipehc@ufps.edu.co>
2022-08-08 16:01:02 -0500

    creando las demas funciones del backend rest

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteRestController.java
@@ -3,9 +3,16 @@ package com.bolsadeideas.springboot.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
@@ -23,4 +30,31 @@ public class ClienteRestController {
 	public List<Cliente> ListarRest() {
 		return clienteService.finAll();
 	}
+	
+	@GetMapping(value="/listar/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findOne(id);
+	}
+	
+	@PostMapping("/add")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void cretae(@RequestBody Cliente cliente) {
+		clienteService.save(cliente);
+	}  
+	
+	@PutMapping("/edit/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void update(@PathVariable Long id, @RequestBody Cliente cliente) {
+		Cliente clienteActual = clienteService.findOne(id);
+		
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		clienteService.save(clienteActual);
+	}
+	
+	
+	
+	
 }
